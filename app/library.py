2d8e3792e78d4741ba07b2a1459daac0d3610b68
Pradeep Kumar <pvpradeepkumar3025@gmail.com>
2026-07-14 14:42:39 +0530

    Delete app/library.py

--- app/library.py
+++
@@ -1,7 +0,0 @@
-#Day 1[Creating Library Attribute]
-
-library_name=input("Enter Library Name:")
-location=input("Enter Library Location:")
-total_books=int(input("Enter Total Number of Books:"))
-total_members=int(input("Enter Total Number of Members:"))
-print("\n")
