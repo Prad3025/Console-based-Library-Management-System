b5a7fa1932c983eee134b268e93d982f26f10c92
Pradeep Kumar <pvpradeepkumar3025@gmail.com>
2026-07-14 14:42:52 +0530

    Delete app/members.py

--- app/members.py
+++
@@ -1,8 +0,0 @@
-# Day 1 [Creating Member Attributes]
-
-member_id=int(input("Enter Member ID:"))
-member_name=input("Enter Member Name:")
-email=input("Enter Member Email:")  
-phone_number=int(input("Enter Member Phone Number:"))
-member_type=input("Enter Membership Type:")
-print("\n")
